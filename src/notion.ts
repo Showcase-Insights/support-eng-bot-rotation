f9deb8b6f20fcf22ac4bd4d123465f8f75971642
Samantha Morrison <samantha.morrison@letshighlight.com>
2026-04-10 15:38:48 -0700

    [Support] update default to 11 weeks?

--- src/notion.ts
+++ src/notion.ts
@@ -64,7 +64,7 @@ export async function writeSignUp(
   });
 }
 
-export async function getRecentlyAssigned(weeks: number = 4): Promise<string[]> {
+export async function getRecentlyAssigned(weeks: number = 11): Promise<string[]> {
   const since = new Date();
   since.setDate(since.getDate() - weeks * 7);
 
